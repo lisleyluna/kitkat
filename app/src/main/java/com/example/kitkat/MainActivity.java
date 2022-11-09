9e4c0bf1bb7e7c359145a8bd6fbe8a84e5087101
MerqueApp-G29 <lisleyluna@gmail.com>
2022-11-08 22:05:08 -0500

    archivo proyecto kitkat sprint1 version1

---
+++ app/src/main/java/com/example/kitkat/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.kitkat;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.widget.TextViewOnReceiveContentListener;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+    }
+}
